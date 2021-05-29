1d4438f95071f225e75c45bf7a4235034f34414d
zhongzihao <642524394@qq.com>
2021-05-29 15:37:10 +0800

    fix some bugs

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install curl
+sudo apt install -y curl
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh --mirror Aliyun
-sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+echo "building docker-compose..."
+curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+sudo mv ./docker-compose /usr/local/bin/
 sudo chmod +x /usr/local/bin/docker-compose
 
